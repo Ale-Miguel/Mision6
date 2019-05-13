629557df23e4850f43b57c3ea8635ad3cb75fcd4
Alejandro Miguel Sanchez <alejandromiguelsanchez@MacBook-Pro-de-Alejandro-5.local>
2019-05-13 00:48:22 -0500

    Server now works with client

--- server.c
+++ server.c
@@ -1,6 +1,6 @@
 /* 
 Alejandro Miguel Sánchez Mora   A01272385  
-Mariano Hurtado
+Mariano Hurtado de Mendoza Carranza A00820039
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,14 +9,22 @@ Mariano Hurtado
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 
 #define BUFFER_SIZE 256 //Constante que guarda el tamaño del buffer
+#define CREATE_FILE "CREATE"
+#define DELETE_FILE "DELETE"
+#define UPDATE_FILE "UPDATE"    
 
 //Función que maneja los errores
 void error(const char *msg){
     perror(msg);
     exit(1);
 }
+
+void createFile(char *sFileName, int newsockfd);
+void updateFile(char *sFileName, int newsockfd);
+void deleteFile(char *sFileName);
 
 int main(int argc, char *argv[]){
 
@@ -30,7 +38,7 @@ int main(int argc, char *argv[]){
 
     struct sockaddr_in serv_addr, cli_addr; 
 
-    int n; //Variable  que almacena el código que regresan las operaciones de mandar y recibir
+    int n =1; //Variable  que almacena el código que regresan las operaciones de mandar y recibir
 
      //El servidor solo pide el número de puerto sobre el que va a escuchar
     if (argc < 2) {
@@ -71,31 +79,104 @@ int main(int argc, char *argv[]){
     if (newsockfd < 0) 
           error("ERROR on accept");
 
-    //Se limpua el buffer
-    bzero(buffer,BUFFER_SIZE);
+    //Se limpia el buffer
 
-    n = read(newsockfd,buffer,BUFFER_SIZE-1);   //Se lee el nombre del archivo
+    char sAction[BUFFER_SIZE];
 
-    //Si no se pudo recibir el nombre del archivo
-    if (n < 0) error("ERROR no se recibio el nombre del archivo");
+    while(n > 0){
 
-    printf("Here is the file name: %s\n",buffer);   //Se imprime el nombre del archivo a la consola
+        bzero(buffer,BUFFER_SIZE);
+        strcpy(sAction, "");
 
-    char sFileName[sizeof(buffer)]; //Arreglo oque almacena el nombre del archivo
+        n = read(newsockfd,buffer,BUFFER_SIZE-1);   //Se lee la accion
 
-    strcpy(sFileName, buffer);  //Se guarda el nombre del archivo
+        if(n <= 0) break;
+
+        strcpy(sAction, buffer);
+
+        bzero(buffer, BUFFER_SIZE);
+
+        n = read(newsockfd,buffer,BUFFER_SIZE-1);   //Se lee el nombre del archivo
+
+        //Si no se pudo recibir el nombre del archivo
+        if (n < 0) error("ERROR no se recibio el nombre del archivo");
+        for(int i = 0; i < BUFFER_SIZE; i++){
+            if(buffer[i] <= 32){
+
+                buffer[i] = 0;
+            }
+        }
+        printf("Here is the file name: %s\n",buffer);   //Se imprime el nombre del archivo a la consola
+
+        if(strcmp(DELETE_FILE, sAction) == 0){
+            printf("DELETE FILE %s\n", buffer);
+            deleteFile(buffer);
+        }
+        else if(strcmp(UPDATE_FILE, sAction) == 0){
+            printf("UPDATE FILE %s\n", buffer);
+            updateFile(buffer, newsockfd);
+        }
+        else{
+            printf("CREATE FILE %s\n", buffer);
+            createFile(buffer, newsockfd);
+        }
+        
+        n = write(newsockfd,"I got your file",18);  //Se envía confirmación al cliente
+
+        /*if (n < 0) error("ERROR writing to socket");*/
+
+    }
+    //Se cierran los sockets
+    close(newsockfd);
+    close(sockfd);
+
+    return 0; 
+}
+
+void createFile(char *sFileName, int newsockfd){
+    //FILE * archivo;
+    int archivo;
+
+    archivo = open(sFileName, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+    close(archivo);
+
+}
+void deleteFile(char *sFileName){
+    remove(sFileName);
+}
+void updateFile(char *sFileName, int newsockfd){
+
+    int n = 0;
+    
+    char buffer[BUFFER_SIZE];  //Buffer que almaena lo que manda y recibe del cliente
+    bzero(buffer, BUFFER_SIZE);
 
     FILE * archivo; //Archivo que se va a crear o modificar
 
-    int words = 0;  //Variable que guarda la cantidad de palabras/segmentos que va a recibir
+
+    char words =0;  //Variable que guarda la cantidad de palabras/segmentos que va a recibir
 
     archivo = fopen(sFileName, "w");  //Se abre el archivo o se crea y se abre
 
-    n = read(newsockfd, &words, sizeof(words)); //Se obtiene la cantidad de segmentos que se van a recibir
 
+    do{
+        bzero(buffer, BUFFER_SIZE);
+        n = read(newsockfd, buffer, BUFFER_SIZE-1); //Se obtiene la cantidad de segmentos que se van a recibir
+
+        words = buffer[0];
+
+        n = write(newsockfd, buffer, BUFFER_SIZE-1);
+
+        bzero(buffer, BUFFER_SIZE);
+
+        n=read(newsockfd, buffer, BUFFER_SIZE-1);
+
+    }while(strcmp(buffer, "OK") != 0);
+
+    
     char sPalabra[BUFFER_SIZE]; //Cadena que guarda el segmento que se recibe 
    
-    //Ciclo para leer los segmentos que envía el cliente    
+    //Ciclo para leer los segmentos que envía el cliente
     for(int i = 1; i <= words; i++){
 
         //Ciclo que se ejecuta hasta recibir el OK del cliente
@@ -117,7 +198,6 @@ int main(int argc, char *argv[]){
         }while(strcmp(buffer, "OK") != 0);  //Se espera que se reciba un OK
 
         fprintf(archivo, "%s", sPalabra);   //Se escribe en el archivo lo que se recibió del cliente
-
         
     }
 
@@ -127,14 +207,7 @@ int main(int argc, char *argv[]){
     if (n < 0) error("ERROR reading to socket");
 
     fclose(archivo);    //Se cierra el archivo
+    //close(archivo);    //Se cierra el archivo
 
-    n = write(newsockfd,"I got your file",18);  //Se envía confirmación al cliente
-
-    if (n < 0) error("ERROR writing to socket");
-
-    //Se cierran los sockets
-    close(newsockfd);
-    close(sockfd);
-    
-    return 0; 
 }
+
